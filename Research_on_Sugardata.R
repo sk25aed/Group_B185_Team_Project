d4035af1997de0f14ced2e46df81fd98b53a1f60
Mohan25-uk <mt25acq@herts.ac.uk>
2025-12-06 23:33:18 +0000

    code for cleaning of data and numeric conversion.

--- Research_on_Sugardata.R
+++ Research_on_Sugardata.R
@@ -8,3 +8,14 @@ summary(df_raw)
 head(df_raw)
 
 ## Cleaning and numeric conversion
+year_cols <- c("2018/19", "2019/20", "2020/21",
+              "2021/22", "2022/23", "May2023/24")
+df_clean <- df_raw |>
+                mutate(
+                  across(
+                    all_of(year_cols),
+                    ~ as.numeric(gsub(",", "", .x))
+                  )
+                )       
+summary(df_clean[, year_cols])
+              
