15ff7d9f1f6c2123b1c59642bb5c8e10c98b2176
HafizFarooqAhmad <ha25acw@herts.ac.uk>
2025-12-06 15:35:18 +0000

    Code for the dataset loading and summery.

---
+++ Dataset Loading & Summar.R
@@ -0,0 +1,14 @@
+
+library(tidyverse)
+library(ggpubr)
+library(GGally)
+
+setwd("F:/RProject")
+
+df <- read_csv("combined_df.csv")
+
+head(df)
+str(df)
+summary(df)
+
+write.csv(summary(df), "data_summary.csv")
