4caff1a492a74d1a7a46ede4160f12eca015e1f9
SanjayKumarAnnu <sa25ait@herts.ac.uk>
2025-12-09 18:06:23 +0000

    Plot For Brazil

--- testingcountry.R
+++ testingcountry.R
@@ -1,1 +1,23 @@
 ##cleaning data for Brazil 
+df_brazil <- df_clean |>
+  filter(Name == "Brazil", Action %in% c("production", "export")) |>
+  pivot_longer(
+    cols      = all_of(year_cols),
+    names_to  = "Year",
+    values_to = "Volume"
+  ) |>
+  pivot_wider(
+    names_from  = Action,
+    values_from = Volume
+  ) |>
+mutate(Year = factor(Year, levels = year_cols))
+
+##Plot For Brazil
+
+plot(df_brazil$production, df_brazil$export,
+     xlab = "Brazil production",
+     ylab = "Brazil export",
+     main = "Brazil Production vs Export")
+
+abline(lm(export ~ production, data = df_brazil),
+       col = "red")
